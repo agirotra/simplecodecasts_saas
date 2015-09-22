fe3b9bd2c33f08062c8d8ee54e3e01d1978f2883
Abhinav Girotra <abhinav.girotra@gmail.com>
2015-09-22 11:33:49 +0000

    Added contacts mailer and View

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,14 @@
+class ContactMailer < ActionMailer::Base
+    default to = 'abhinav.girotra@gmail.com'
+    
+    def contact_email(name, email, body)
+        @name = name
+        @email = email
+        @body = body 
+        
+        mail(from: email, subject: 'Ruby Contact Form Message')
+    
+    end
+    
+
+end
