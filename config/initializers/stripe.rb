c289aea685aa0edf10598838bf49b5206d4244ac
Abhinav Girotra <abhinav.girotra@gmail.com>
2015-10-27 06:06:57 +0000

    changed api status again

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,3 +1,4 @@
 Stripe.api_key = ENV["stripe_api_key"]
 STRIPE_PUBLIC_KEY = ENV["stripe_publishable_key"]
-Stripe.api_version = '2015-01-11'  #Making sure the API version used is compatible.
+#Stripe.api_version = '2015-01-11'  #Making sure the API version used is compatible.
+Stripe.api_version = '2015-02-16'
