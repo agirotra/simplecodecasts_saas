d1d20ed93f29c29fce0cb5d4043095d4926522c1
Abhinav Girotra <abhinav.girotra@gmail.com>
2015-10-27 06:01:05 +0000

    changed Strip API version

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,3 +1,3 @@
 Stripe.api_key = ENV["stripe_api_key"]
 STRIPE_PUBLIC_KEY = ENV["stripe_publishable_key"]
-Stripe.api_version = '2014-12-17'
+Stripe.api_version = '2015-01-11'  #Making sure the API version used is compatible.
