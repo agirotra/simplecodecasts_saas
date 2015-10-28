3cb5f9b4a9da8dbdbfcee88ebf12dafdf1d4b354
Abhinav Girotra <abhinav.girotra@gmail.com>
2015-10-28 10:32:27 +0000

    commenting save for testing in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
       customer = Stripe::Customer.create(description: email, plan: plan_id, source: stripe_card_token)
       self.stripe_customer_token = customer.id
 
-      save!
+     # save!
     end
   end
 end
